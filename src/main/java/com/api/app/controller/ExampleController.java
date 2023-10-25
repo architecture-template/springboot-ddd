7f985820f89bcad06c1ffbe889b49917ad6ef313
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-10-25 16:34:20 +0900

    pathをグループ化する処理を追加

--- src/main/java/com/api/app/controller/ExampleController.java
+++ src/main/java/com/api/app/controller/ExampleController.java
@@ -1,17 +1,17 @@
 package com.api.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.api.app.response.ApiResponse;
 import com.api.domain.model.Example;
 import com.api.domain.service.ExampleService;
 
-@Controller
 @RestController
+@RequestMapping("/example")
 public class ExampleController {
 
     private final ExampleService exampleService;
@@ -21,7 +21,7 @@ public class ExampleController {
         this.exampleService = exampleService;
     }
 
-    @GetMapping("/example/{id}")
+    @GetMapping("/{id}")
     public ApiResponse getExample(@PathVariable Long id) {
         Example example = exampleService.getExampleById(id);
         if (example == null) {
